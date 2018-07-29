10b84b04c5f80d8e60b6f5e5ede3886fd22ce72e
Killeroo <matthewcarney64@gmail.com>
2018-07-29 18:50:36 +0100

    Added PotatoBot/Stats.cs

---
+++ PotatoBot/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PotatoBot
+{
+    /// <summary>
+    /// Stores all potatobots stats
+    /// </summary>
+    public static class Stats
+    {
+
+    }
+}
