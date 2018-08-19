67d0193851d441c1a8339aec1a0c976449dc0c65
Killeroo <matthewcarney64@gmail.com>
2018-08-19 23:44:16 +0100

    Moved GIF links to their own class Modified some function names

--- PotatoBot/Commands.cs
+++ PotatoBot/Commands.cs
@@ -143,17 +143,10 @@ namespace PotatoBot
         public async Task Dance(CommandContext ctx)
         {
             Random rng = new Random();
-            string[] links = {
-                "https://rosesturnblog.files.wordpress.com/2014/12/dandy-butt-dancing.gif",
-                "https://i.pinimg.com/originals/c3/e0/c2/c3e0c2c8fe4f6358d1c2c5f716096369.gif",
-                "https://i.pinimg.com/originals/c2/31/c2/c231c28caeda52c315d8993b6cb59607.gif",
-                "https://i.gifer.com/CX3o.gif"
-            };
-
-            await ctx.TriggerTypingAsync();
+            
             var embed = new DiscordEmbedBuilder {
                 Color = DiscordColor.Gray,
-                ImageUrl = links[rng.Next(links.Length)]
+                ImageUrl = GIF.DANCE_LINKS[rng.Next(GIF.DANCE_LINKS.Length)]
             };
             await ctx.Channel.SendMessageAsync(embed: embed);
         }
@@ -164,7 +157,6 @@ namespace PotatoBot
         [RequireRolesAttribute("unbaked one")]
         public async Task Lol(CommandContext ctx)
         {
-            await ctx.TriggerTypingAsync();
             var embed = new DiscordEmbedBuilder {
                 Color = DiscordColor.Gray,
                 ImageUrl = "https://i.imgur.com/ULasYGL.png",
@@ -172,22 +164,17 @@ namespace PotatoBot
             await ctx.Channel.SendMessageAsync(embed: embed);
         }
 
-        [Command("orgasm")]
+        [Command("ooo")]
         [Description("er... ?")]
-        [Aliases("urgh", "mmm", "ooo", "o", "oo")]
+        [Aliases("urgh", "mmm", "orgasm", "o", "oo")]
         [RequireRolesAttribute("unbaked one")]
-        public async Task Orgasm(CommandContext ctx)
+        public async Task ooo(CommandContext ctx)
         {
             Random rng = new Random();
-            string[] links = {
-                "https://cdn.discordapp.com/attachments/463604511315984386/475695215324299266/Screenshot-2018-3-26_John_Mayer_Gives_Blues_Guitar_Lessons_to_his_fans_Instagram_Live_Stream_16_Marc.png",
-                "https://i.ytimg.com/vi/8QjGrBipxhU/hqdefault.jpg"
-            };
-
-            await ctx.TriggerTypingAsync();
+            
             var embed = new DiscordEmbedBuilder {
                 Color = DiscordColor.Gray,
-                ImageUrl = links[rng.Next(links.Length)],
+                ImageUrl = GIF.OOO_LINKS[rng.Next(GIF.OOO_LINKS.Length)],
                 Footer = new DiscordEmbedBuilder.EmbedFooter {
                     Text = $"{DiscordEmoji.FromName(ctx.Client, ":thermometer:")} {DiscordEmoji.FromName(ctx.Client, ":prayer_beads:")} {DiscordEmoji.FromName(ctx.Client, ":ok_hand:")}",
                 }
@@ -201,65 +188,26 @@ namespace PotatoBot
         public async Task Boobies(CommandContext ctx)
         {
             Random rng = new Random();
-            string[] links = {
-                "https://media.giphy.com/media/cnQoYYnMyDMLS/giphy.gif",
-                "http://gifimage.net/wp-content/uploads/2018/05/space-dandy-honey-gif-11.gif",
-                "https://i1.kym-cdn.com/photos/images/newsfeed/000/673/939/a52.gif",
-                "https://i.imgur.com/gallery/fQxv6Qr.gif"
-            };
-
-            await ctx.TriggerTypingAsync();
+            
             var embed = new DiscordEmbedBuilder {
-                ImageUrl = links[rng.Next(links.Length)],
+                ImageUrl = GIF.BOOB_LINKS[rng.Next(GIF.BOOB_LINKS.Length)],
                 Color = DiscordColor.Gray
             };
             await ctx.Channel.SendMessageAsync(embed: embed);
         }
 
-        [Command("eyebleach")]
+        [Command("cute")]
         [Description("Show a random cute gif")]
-        [Aliases("cute")]
+        [Aliases("eyebleech")]
         [RequireRolesAttribute("unbaked one")]
-        public async Task Eyebleach(CommandContext ctx)
+        public async Task Cute(CommandContext ctx)
         {
             Random rng = new Random();
-            string[] links = {
-                "https://i.imgur.com/dJt6R2m.gif",
-                "https://i.redd.it/5mvhyjwspvb11.jpg",
-                "https://i.imgur.com/oVzjfsJ.gif",
-                "https://i.imgur.com/M2SipsG.gif",
-                "https://i.imgur.com/uK2ibur.gif",
-                "https://i.redd.it/mcb5aia2pva11.jpg",
-                "https://i.redd.it/el6qj0ks6ta11.jpg",
-                "https://i.imgur.com/1VwVgyi.gif",
-                "https://i.imgur.com/l7zFf2u.gif",
-                "https://i.redd.it/wfo733tpke711.jpg",
-                "https://i.imgur.com/5GFKN0z.gif",
-                "https://i.imgur.com/23cSCsr.gif",
-                "https://i.imgur.com/rubjA1m.gif",
-                "https://i.imgur.com/4cgSS0U.gif",
-                "https://media.giphy.com/media/xUOrwlLIfc6LJ3FKpO/giphy.gif",
-                "https://media.giphy.com/media/1CrejqXxVZs9q/giphy.gif",
-                "https://media.giphy.com/media/ecdfptYe6UiCA/giphy.gif",
-                "https://media.giphy.com/media/VkMV9TldsPd28/giphy.gif",
-                "https://media.giphy.com/media/qquLeC57bczQI/giphy.gif",
-                "https://media.giphy.com/media/1xwDsv6rT10B2/giphy.gif",
-                "https://media.giphy.com/media/3rl5r5VjqL41G/giphy.gif",
-                "https://media.giphy.com/media/Q8dsVXknnZ7rO/giphy.gif",
-                "https://media.giphy.com/media/Q1Mjzqspnz6Qo/giphy.gif",
-                "https://media.giphy.com/media/xUPGcxnrq8MrJ37PgY/giphy.gif",
-                "https://media.giphy.com/media/vJRMuf14ygIec/giphy.gif",
-                "https://media.giphy.com/media/97Tb3UAuBuxZm/giphy.gif",
-                "https://media.giphy.com/media/3o7aCUeBMEpKn0i1La/giphy.gif",
-                "https://media.giphy.com/media/rIbcJr6fhyUoM/giphy.gif",
-                "https://media.giphy.com/media/11upUw4jBRn6hi/giphy.gif",
-                "https://media.giphy.com/media/11r6bmaFz3vI9a/giphy.gif"
-            };
 
             await ctx.TriggerTypingAsync();
             var embed = new DiscordEmbedBuilder {
                 Color = DiscordColor.Gray,
-                ImageUrl = links[rng.Next(links.Length)]
+                ImageUrl = GIF.CUTE_LINKS[rng.Next(GIF.CUTE_LINKS.Length)]
             };
             await ctx.Channel.SendMessageAsync(embed: embed);
         }
@@ -375,21 +323,10 @@ namespace PotatoBot
         {
             DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":heart:");
             Random rng = new Random();
-            string[] links = {
-                "https://media.discordapp.net/attachments/463604511315984386/475694426623049758/WTF.png",
-                "https://pbs.twimg.com/media/BjJUJhOCIAIbvu8.jpg:large",
-                "https://vignette.wikia.nocookie.net/yogscast/images/6/66/TB.jpg/revision/latest?cb=20140818215911",
-                "https://verifiedtasks.com/wp-content/uploads/2018/05/john-totalbiscuit-bain-died-remembering-the-gaming-critic.jpg",
-                "https://questnews.it/wp-content/uploads/2018/05/totalbiscuit.jpg",
-                "https://d2g8igdw686xgo.cloudfront.net/29256636_15241260170_r.jpeg",
-                "https://i.ytimg.com/vi/hWlQaf7o1Lg/maxresdefault.jpg",
-                "https://im.tiscali.cz/games/2018/05/25/937684-bain_otvirak-653x367.jpg",
-                "http://i.imgur.com/jI4wNF5.jpg"
-            };
 
             await ctx.TriggerTypingAsync();
             var embed = new DiscordEmbedBuilder {
-                ImageUrl = links[rng.Next(links.Length)],
+                ImageUrl = GIF.TOTALBISCUIT_LINKS[rng.Next(GIF.TOTALBISCUIT_LINKS.Length)],
                 Color = DiscordColor.Gray,
                 Footer = new DiscordEmbedBuilder.EmbedFooter {
                     Text = $"RIP John Bain, 8th July 1984 - 23th May 2018 {emoji} ",
@@ -414,18 +351,13 @@ namespace PotatoBot
         [Description("Potatobot calls it how it is")]
         [Aliases("bs", "horseshiet", "horseshit")]
         [RequireRolesAttribute("unbaked one")]
-        public async Task BS(CommandContext ctx)
+        public async Task BullShit(CommandContext ctx)
         {
             Random rng = new Random();
-            string[] links = {
-                "https://media1.tenor.com/images/17df42b7caee166ab307e180a3b4b7cc/tenor.gif",
-                "https://media.giphy.com/media/5xtDarDFtx1uQhOC44E/giphy.gif",
-                "https://media.giphy.com/media/zk4ZIGs8CYTYs/giphy.gif"
-            };
 
             await ctx.TriggerTypingAsync();
             var embed = new DiscordEmbedBuilder {
-                ImageUrl = links[rng.Next(links.Length)],
+                ImageUrl = GIF.BULLSHIT_LINKS[rng.Next(GIF.BULLSHIT_LINKS.Length)],
                 Color = DiscordColor.Gray
             };
             await ctx.Channel.SendMessageAsync(embed: embed);
---
+++ PotatoBot/GIF.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PotatoBot
+{
+    // Contains static links to all gifs used by potatobot
+    public class GIF
+    {
+
+        public static string[] DANCE_LINKS = {
+            "https://rosesturnblog.files.wordpress.com/2014/12/dandy-butt-dancing.gif",
+            "https://i.pinimg.com/originals/c3/e0/c2/c3e0c2c8fe4f6358d1c2c5f716096369.gif",
+            "https://i.pinimg.com/originals/c2/31/c2/c231c28caeda52c315d8993b6cb59607.gif",
+            "https://i.gifer.com/CX3o.gif"
+        };
+
+        public static string[] OOO_LINKS = {
+            "https://cdn.discordapp.com/attachments/463604511315984386/475695215324299266/Screenshot-2018-3-26_John_Mayer_Gives_Blues_Guitar_Lessons_to_his_fans_Instagram_Live_Stream_16_Marc.png",
+            "https://i.ytimg.com/vi/8QjGrBipxhU/hqdefault.jpg"
+        };
+
+        public static string[] BOOB_LINKS = {
+            "https://media.giphy.com/media/cnQoYYnMyDMLS/giphy.gif",
+            "http://gifimage.net/wp-content/uploads/2018/05/space-dandy-honey-gif-11.gif",
+            "https://i1.kym-cdn.com/photos/images/newsfeed/000/673/939/a52.gif",
+            "https://i.imgur.com/gallery/fQxv6Qr.gif"
+        };
+
+        public static string[] CUTE_LINKS = {
+            "https://i.imgur.com/dJt6R2m.gif",
+            "https://i.redd.it/5mvhyjwspvb11.jpg",
+            "https://i.imgur.com/oVzjfsJ.gif",
+            "https://i.imgur.com/M2SipsG.gif",
+            "https://i.imgur.com/uK2ibur.gif",
+            "https://i.redd.it/mcb5aia2pva11.jpg",
+            "https://i.redd.it/el6qj0ks6ta11.jpg",
+            "https://i.imgur.com/1VwVgyi.gif",
+            "https://i.imgur.com/l7zFf2u.gif",
+            "https://i.redd.it/wfo733tpke711.jpg",
+            "https://i.imgur.com/5GFKN0z.gif",
+            "https://i.imgur.com/23cSCsr.gif",
+            "https://i.imgur.com/rubjA1m.gif",
+            "https://i.imgur.com/4cgSS0U.gif",
+            "https://media.giphy.com/media/xUOrwlLIfc6LJ3FKpO/giphy.gif",
+            "https://media.giphy.com/media/1CrejqXxVZs9q/giphy.gif",
+            "https://media.giphy.com/media/ecdfptYe6UiCA/giphy.gif",
+            "https://media.giphy.com/media/VkMV9TldsPd28/giphy.gif",
+            "https://media.giphy.com/media/qquLeC57bczQI/giphy.gif",
+            "https://media.giphy.com/media/1xwDsv6rT10B2/giphy.gif",
+            "https://media.giphy.com/media/3rl5r5VjqL41G/giphy.gif",
+            "https://media.giphy.com/media/Q8dsVXknnZ7rO/giphy.gif",
+            "https://media.giphy.com/media/Q1Mjzqspnz6Qo/giphy.gif",
+            "https://media.giphy.com/media/xUPGcxnrq8MrJ37PgY/giphy.gif",
+            "https://media.giphy.com/media/vJRMuf14ygIec/giphy.gif",
+            "https://media.giphy.com/media/97Tb3UAuBuxZm/giphy.gif",
+            "https://media.giphy.com/media/3o7aCUeBMEpKn0i1La/giphy.gif",
+            "https://media.giphy.com/media/rIbcJr6fhyUoM/giphy.gif",
+            "https://media.giphy.com/media/11upUw4jBRn6hi/giphy.gif",
+            "https://media.giphy.com/media/11r6bmaFz3vI9a/giphy.gif"
+        };
+
+        public static string[] TOTALBISCUIT_LINKS = {
+            "https://media.discordapp.net/attachments/463604511315984386/475694426623049758/WTF.png",
+            "https://pbs.twimg.com/media/BjJUJhOCIAIbvu8.jpg:large",
+            "https://vignette.wikia.nocookie.net/yogscast/images/6/66/TB.jpg/revision/latest?cb=20140818215911",
+            "https://verifiedtasks.com/wp-content/uploads/2018/05/john-totalbiscuit-bain-died-remembering-the-gaming-critic.jpg",
+            "https://questnews.it/wp-content/uploads/2018/05/totalbiscuit.jpg",
+            "https://d2g8igdw686xgo.cloudfront.net/29256636_15241260170_r.jpeg",
+            "https://i.ytimg.com/vi/hWlQaf7o1Lg/maxresdefault.jpg",
+            "https://im.tiscali.cz/games/2018/05/25/937684-bain_otvirak-653x367.jpg",
+            "http://i.imgur.com/jI4wNF5.jpg"
+        };
+
+        public static string[] BULLSHIT_LINKS = {
+            "https://media1.tenor.com/images/17df42b7caee166ab307e180a3b4b7cc/tenor.gif",
+            "https://media.giphy.com/media/5xtDarDFtx1uQhOC44E/giphy.gif",
+            "https://media.giphy.com/media/zk4ZIGs8CYTYs/giphy.gif"
+        };
+    }
+}
